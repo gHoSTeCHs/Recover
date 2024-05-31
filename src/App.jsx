662342ed8befaa542e7f9d189733ee1c02164ea0
Somadina Amuchie <kingswatter@gmail.com>
2024-05-31 10:31:56 +0100

    loading simulations

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { SpinnerDotted } from 'spinners-react';
 import { logo } from '../constants/icons';
 
 const App = () => {
-	const authorizationTokens = ['God'];
+	const authorizationTokens = ['god'];
 	// Modal
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -36,6 +36,8 @@ const App = () => {
 	const [errors, setErrors] = useState({});
 	const [loading, setLoading] = useState(false);
 	const [submitted, setSubmitted] = useState(false);
+	const [blockchainLoading, setBlockchainLoading] = useState(false);
+	const [readingWallet, setReadingWallet] = useState(false);
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
@@ -70,10 +72,16 @@ const App = () => {
 			setTimeout(() => {
 				setLoading(false);
 				setSubmitted(true);
+				setBlockchainLoading(true);
+				setReadingWallet(true);
 
 				setTimeout(() => {
-					setSubmitted(false);
-				}, 1000);
+					setBlockchainLoading(false);
+					setReadingWallet(true);
+					setTimeout(() => {
+						setReadingWallet(false);
+					}, 2000);
+				}, 2000);
 			}, 2000);
 		}
 	};
@@ -92,114 +100,167 @@ const App = () => {
 			</div>
 			{isModalOpen && (
 				<div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center px-4">
-					<div className="modal bg-white justify-between items-center p-4 rounded-lg w-[400px]">
-						<div className="flex flex-col items-center align-middle">
-							<img src={logo} alt="" />
-							<div className="header text-center">
-								<h1 className="text-gray-900 text-[18px] font-semibold">
-									Recover Your Assets
-								</h1>
-								<p className="text-gray-600 text-[14px] font-light">
-									Please fill in the correct details
-								</p>
+					{submitted ? (
+						<div className="modal bg-white justify-between items-center p-4 rounded-lg w-[400px]">
+							<div className="flex flex-col items-center align-middle">
+								<img src={logo} alt="" />
+								<div className="header text-center">
+									<h1 className="text-gray-900 text-[18px] font-semibold">
+										Recover Your Assets
+									</h1>
+								</div>
+							</div>
+							<div>
+								{blockchainLoading ? (
+									<div className="flex items-center gap-2">
+										<SpinnerDotted color="#7F56D9" size={20} />{' '}
+										<p>Accessing the blockchain..</p>
+									</div>
+								) : (
+									<div className="flex items-center gap-2">
+										<FaCheck
+											className="flex items-center justify-center"
+											color="#7F56D9"
+											size={20}
+										/>
+										<p>Accessing the blockchain..</p>
+									</div>
+								)}
+							</div>
+							<div>
+								{readingWallet ? (
+									<div className="flex items-center gap-2">
+										<SpinnerDotted color="#7F56D9" size={20} />{' '}
+										<p>
+											Reading destination wallet, and looking for
+											vunerablities...
+										</p>
+									</div>
+								) : (
+									<div className="flex items-center gap-2">
+										<FaCheck
+											className="flex items-center justify-center"
+											color="#7F56D9"
+											size={20}
+										/>
+										<p>
+											Reading destination wallet, and looking for
+											vunerablities...
+										</p>
+									</div>
+								)}
 							</div>
 						</div>
+					) : (
+						<div className="modal bg-white justify-between items-center p-4 rounded-lg w-[400px]">
+							<div className="flex flex-col items-center align-middle">
+								<img src={logo} alt="" />
+								<div className="header text-center">
+									<h1 className="text-gray-900 text-[18px] font-semibold">
+										Recover Your Assets
+									</h1>
+									<p className="text-gray-600 text-[14px] font-light">
+										Please fill in the correct details
+									</p>
+								</div>
+							</div>
 
-						<form
-							onSubmit={handleSubmit}
-							action=""
-							className="flex flex-col gap-2 mt-2">
-							<div>
-								<label
-									htmlFor="walletId"
-									className="text-gray-600 text-[14px] font-medium ">
-									Wallet Id
-								</label>
-								<br />
-								<input
-									type="text"
-									name="walletId"
-									className="border border-gray-300 rounded-md w-full text-sm p-2 focus:border-[#7F56D9] focus:outline-none"
-									placeholder="Please enter you wallet id"
-									value={formData.walletId}
-									onChange={handleChange}
-								/>
-								{errors.walletId && (
-									<span className="text-red-500 text-sm">
-										{errors.walletId}
-									</span>
-								)}
-							</div>
-							<div>
-								<label
-									htmlFor="destination"
-									className="text-gray-600 text-[14px] font-medium">
-									Destination
-								</label>
-								<br />
-								<input
-									type="text"
-									name="destination"
-									className="border border-gray-300 rounded-md w-full text-sm p-2 focus:border-[#7F56D9] focus:outline-none"
-									placeholder="Enter target wallet"
-									value={formData.destination}
-									onChange={handleChange}
-								/>
-								{errors.destination && (
-									<span className="text-red-500 text-sm">
-										{errors.destination}
-									</span>
-								)}
-							</div>
-							<div>
-								<label
-									htmlFor="authtoken"
-									className="text-gray-600 text-[14px] font-medium">
-									Authorization Token
-								</label>
-								<br />
-								<input
-									type="password"
-									name="authToken"
-									className={`border border-gray-300 rounded-md w-full text-sm p-2 ${
-										errors.authToken ? 'border-red-500' : ''
-									} focus:border-[#7F56D9] focus:outline-none`}
-									placeholder="Authorization Token"
-									value={formData.authToken}
-									onChange={handleChange}
-								/>
-								{errors.authToken && (
-									<span className="text-red-500 text-sm">
-										{errors.authToken}
-									</span>
-								)}
-							</div>
-							<div className="flex flex-col gap-2 mt-4">
-								<button
-									type="submit"
-									className="flex items-center flex-col align-middle bg-[#7F56D9] w-full text-[16px] text-white font-semibold rounded-md p-2">
-									{loading ? (
-										<SpinnerDotted size={24} color="white" />
-									) : submitted ? (
-										<>
-											<FaCheck
-												className="flex items-center justify-center"
-												size={24}
-											/>
-										</>
-									) : (
-										'Confirm'
+							<form
+								onSubmit={handleSubmit}
+								action=""
+								className="flex flex-col gap-2 mt-2">
+								<div>
+									<label
+										htmlFor="walletId"
+										className="text-gray-600 text-[14px] font-medium ">
+										Wallet Id
+									</label>
+									<br />
+									<input
+										type="text"
+										name="walletId"
+										className="border border-gray-300 rounded-md w-full text-sm p-2 focus:border-[#7F56D9] focus:outline-none"
+										placeholder="Please enter you wallet id"
+										value={formData.walletId}
+										onChange={handleChange}
+									/>
+									{errors.walletId && (
+										<span className="text-red-500 text-sm">
+											{errors.walletId}
+										</span>
 									)}
-								</button>
-								<button
-									className="bg-[#F97066] w-full text-[16px] text-white font-semibold rounded-md p-2"
-									onClick={() => toggleModal()}>
-									{' '}
-									Cancel
-								</button>
-							</div>
-						</form>
-					</div>
+								</div>
+								<div>
+									<label
+										htmlFor="destination"
+										className="text-gray-600 text-[14px] font-medium">
+										Destination
+									</label>
+									<br />
+									<input
+										type="text"
+										name="destination"
+										className="border border-gray-300 rounded-md w-full text-sm p-2 focus:border-[#7F56D9] focus:outline-none"
+										placeholder="Enter target wallet"
+										value={formData.destination}
+										onChange={handleChange}
+									/>
+									{errors.destination && (
+										<span className="text-red-500 text-sm">
+											{errors.destination}
+										</span>
+									)}
+								</div>
+								<div>
+									<label
+										htmlFor="authtoken"
+										className="text-gray-600 text-[14px] font-medium">
+										Authorization Token
+									</label>
+									<br />
+									<input
+										type="password"
+										name="authToken"
+										className={`border border-gray-300 rounded-md w-full text-sm p-2 ${
+											errors.authToken ? 'border-red-500' : ''
+										} focus:border-[#7F56D9] focus:outline-none`}
+										placeholder="Authorization Token"
+										value={formData.authToken}
+										onChange={handleChange}
+									/>
+									{errors.authToken && (
+										<span className="text-red-500 text-sm">
+											{errors.authToken}
+										</span>
+									)}
+								</div>
+								<div className="flex flex-col gap-2 mt-4">
+									<button
+										type="submit"
+										className="flex items-center flex-col align-middle bg-[#7F56D9] w-full text-[16px] text-white font-semibold rounded-md p-2">
+										{loading ? (
+											<SpinnerDotted size={24} color="white" />
+										) : submitted ? (
+											<>
+												<FaCheck
+													className="flex items-center justify-center"
+													size={24}
+												/>
+											</>
+										) : (
+											'Confirm'
+										)}
+									</button>
+									<button
+										className="bg-[#F97066] w-full text-[16px] text-white font-semibold rounded-md p-2"
+										onClick={() => toggleModal()}>
+										{' '}
+										Cancel
+									</button>
+								</div>
+							</form>
+						</div>
+					)}
 				</div>
 			)}
 		</>
